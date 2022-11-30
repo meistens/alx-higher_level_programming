ba9e06512e785ad39e76dcba52e317ac1ac40cfc
David Mebo <mebodave@gmail.com>
2022-11-29 16:12:05 -0800

    numbers, spelling error and checker specifications met

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 100):
     if number == 99:
-        print(f"{number:d}")
+        print("{}".format(number))
     else:
-        print(f"{number:2d}", end=", ")
+        print("{:02}".format(number), end=", ")
