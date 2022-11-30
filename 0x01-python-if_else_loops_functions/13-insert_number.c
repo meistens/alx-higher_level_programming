f32a26e36fa1b214ae08ca1003e4d9514b688ec8
David Mebo <mebodave@gmail.com>
2022-11-30 13:55:06 -0800

    digits, fizzbuzz and linked list

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * insert_node - insert node in a sorted linked list
+ * @head: head of the linked list
+ * @number: user input/node
+ * Return: address of the node or NULL
+ *
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *node = *head;
+	listint_t *node = *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+
+	if (node == NULL || node->n >= number)
+	{
+		new->next = node;
+		*head = new;
+		return (new);
+	} else
+	{
+		while (node && node->next && node->next->n < number)
+			node = node->next;
+		new->next = node->next;
+		node->next = new;
+		return (new);
+	}
+}
