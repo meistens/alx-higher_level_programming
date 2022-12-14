a83c9096b59a56b51fdbe1a3ea5a9d1286d8bb82
David Mebo <mebodave@gmail.com>
2022-12-14 05:53:33 -0800

    tasks

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ""
-    for i in my_string:
-        if i != 'c' or i != 'C':
-            new_string += i
-            return new_string
+    str = [i for i in my_string if i != 'c' or i != 'C']
+    return ("".join(str))
