cb80d005fa9b9a60a8d2a463d4859eabf6854246
David Mebo <mebodave@gmail.com>
2022-12-18 06:00:04 -0800

    small fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     new_str = ""
     for i in my_string:
-        if i != 'c' or i != 'C':
-            new_str += i
-
+        if i in ['c', 'C']:
+            continue
+        new_str += i
     return new_str
