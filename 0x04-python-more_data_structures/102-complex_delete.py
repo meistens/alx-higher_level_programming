15778b9445a3e3132155cc88648c864598c047a3
David Mebo <mebodave@gmail.com>
2022-12-18 05:45:07 -0800

    done my best, need to cooldown

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,6 @@
+def complex_delete(a_dictionary, value):
+    new_dict = a_dictionary.copy()
+    for key, v in new_dict.items():
+        if v == value:
+            del a_dictionary[key]
+    return a_dictionary
