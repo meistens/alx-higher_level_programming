501b08f28a62e0a4f2c2eac05510537fb2530f4d
David Mebo <mebodave@gmail.com>
2022-12-21 16:10:08 -0800

    requirement check

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""Define a class Square."""
 class Square:
+    """Represent a square."""
     def __init__(self, size=0):
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+        """
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -9,6 +15,7 @@ class Square:
 
         @property
         def size(self):
+            """Get/set the current size of the square."""
             return self.__size
 
         @size.setter
@@ -20,4 +27,5 @@ class Square:
             self.__size = value
 
         def area(self):
+            """Return the current area of the square."""
             return self.__size * self.__size
--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""Define a class Square."""
 class Square:
+    """Represent a square."""
     def __init__(self, size=0):
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+        """
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -9,10 +15,12 @@ class Square:
 
         @property
         def size(self):
+            """Get/set the current size of the square."""
             return self.__size
 
         @size.setter
         def size(self, value):
+            """Set the current value of the square."""
             if type(value) is not int:
                 raise TypeError("size must be an integer")
             elif value < 0:
@@ -20,9 +28,11 @@ class Square:
             self.__size = value
 
         def area(self):
+            """Return the current area of the square."""
             return self.__size * self.__size
 
         def my_print(self):
+            """Print the square with the # character."""
             if self.__size is 0:
                 print()
             else:
